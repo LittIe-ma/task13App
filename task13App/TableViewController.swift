328d98dfc7dc421feeb7049dea2cc64f8c09b6dd
yasu1125 <v22.m1125@gmail.com>
2021-04-25 16:58:00 +0900

    initial commit

---
+++ task13App/TableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TableViewController.swift
+//  task13App
+//
+//  Created by yasudamasato on 2021/04/23.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    private var checklistItem = ["りんご", "みかん", "バナナ", "パイナップル"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        checklistItem.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
+
+        let imageView = cell.viewWithTag(1) as! UIImageView
+
+        if indexPath.row % 2 == 1 {
+            imageView.image = UIImage(named: "check")
+        }
+
+        let label = cell.viewWithTag(2) as! UILabel
+
+        label.text = checklistItem[indexPath.row]
+
+        return cell
+    }
+}
